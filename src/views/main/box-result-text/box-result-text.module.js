a25db9824ad81fef20f12fefa5edb2548ac4cd16
Jefferon Alves de Aguiar <jeffoaguiar@gmail.com>
2024-02-27 15:23:03 -0300

    implementando função copy

--- src/views/main/box-result-text/box-result-text.module.js
+++ src/views/main/box-result-text/box-result-text.module.js
@@ -1,5 +1,7 @@
-const btn__copy = document.querySelector('.btn__copy');
+import { textarea__result } from "../../../components/textarea/text-result/text-result.textarea.js";
+import { copy } from "../../../controller/copy/copy.controller.js";
+import { btn__copy } from "../../../components/button/copy/copy.button.js";
 
-btn__copy.addEventListener('click', ()=>{
-    console.log('clicou em copiar');
+btn__copy.addEventListener('click', () => {
+    copy(textarea__result.value);
 });
