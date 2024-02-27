47cf0f5c204b198ac516593c870cb9f1b975c5c5
Jefferon Alves de Aguiar <jeffoaguiar@gmail.com>
2024-02-27 16:03:48 -0300

    adicionando title 'Decodificador' em header

--- src/views/header/header.js
+++ src/views/header/header.js
@@ -3,4 +3,9 @@ const nav__bar = document.querySelector('.nav__bar');
 const img = document.createElement('img');
 img.src = './src/assets/icon/icons8-cool-64 (1).png';
 
+const title = document.createElement('h1');
+title.classList.add('title');
+title.innerText = 'Decodificador de textos';
+
 nav__bar.appendChild(img);
+nav__bar.appendChild(title);
